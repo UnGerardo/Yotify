f027c37aaa88bed6e40d23cb9cf4608a1b0cad71
UnGerardo <gerardojdls@gmail.com>
2024-06-04 20:03:10 -0700

    Implemented endpoint to retrieve access_token from spotify API

--- controllers/server.mjs
+++ controllers/server.mjs
@@ -1,24 +1,27 @@
+import 'dotenv/config';
 import { createServer } from 'node:http';
 import { readFile } from 'node:fs';
+import fetch from 'node-fetch';
 
-const server = createServer((req, res) => {
+const server = createServer(async (req, res) => {
   let path;
 
   if (req.url.includes('js')) {
     res.setHeader('Content-Type', 'text/javascript');
     console.log(req.url)
-    path = req.url;
+    path = `.${req.url}`;
   }
   else if (req.url.includes('css')) {
     res.setHeader('Content-Type', 'text/css');
     console.log(req.url)
-    path = req.url;
+    path = `.${req.url}`;
   }
   else {
     res.setHeader('Content-Type', 'text/html');
     path = './html/';
 
     switch(req.url) {
+      // html
       case '/':
         res.statusCode = 200;
         path += 'index.html';
@@ -27,12 +30,35 @@ const server = createServer((req, res) => {
         res.statusCode = 200;
         path += 'spotSearch.html';
         break;
+
+      // endpoints
+      case '/spotToken':
+        const params = new URLSearchParams();
+        params.append('grant_type', 'client_credentials');
+        params.append('client_id', process.env.CLIENT_ID);
+        params.append('client_secret', process.env.CLIENT_SECRET);
+
+        const response = await fetch('https://accounts.spotify.com/api/token', {
+          method: 'POST',
+          body: params,
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        });
+
+        const data = await response.json();
+
+        console.log(data);
+        res.end(JSON.stringify({
+          req: 'Complete',
+        }));
+        return;
+        break;
       default:
         res.statusCode = 404;
         path += '404.html';
     }
   }
 
+  console.log(path)
   readFile(path, (err, data) => {
     if (err) {
       console.log(err);
