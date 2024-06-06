94da348a41c477c3a2b4824cf0a6272f5a58a0e4
UnGerardo <gerardojdls@gmail.com>
2024-06-05 22:26:01 -0700

    Implemented client side fetch() to /spotToken endpoint

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
+let spotifyAccessToken = '';
+let spotifyTokenType = '';
+let spotifyTokenExpiration = 0;
 
+// naming scheme to differentiatie DOM elements and global vars? from normal vars
 const searchBtn = document.getElementById('searchBtn');
+const searchQueryInput = document.getElementById('searchInput');
 
 searchBtn.addEventListener('click', async () => {
-  const response = await fetch('/spotToken');
-  const data = await response.json();
-  console.log(data);
+  if (spotifyAccessToken === '' || Date.now() > spotifyTokenExpiration) {
+    const response = await fetch('/spotToken');
+    const data = await response.json();
+    console.log(data);
+
+    spotifyAccessToken = data['access_token'];
+    spotifyTokenType = data['token_type'];
+    spotifyTokenExpiration = Date.now() + 3000000;
+  }
 });
