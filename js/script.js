4a0e4278b993e0ab991667235481bd67b99b7e61
UnGerardo <gerardojdls@gmail.com>
2024-06-05 23:04:19 -0700

    Created renderSearchResult func to display search results in HTML

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ let spotifyTokenExpiration = 0;
 // naming scheme to differentiatie DOM elements and global vars? from normal vars
 const searchBtn = document.getElementById('searchBtn');
 const searchQueryInput = document.getElementById('searchInput');
+const searchResultsSect = document.getElementById('searchResults');
 
 searchBtn.addEventListener('click', async () => {
   if (spotifyAccessToken === '' || Date.now() > spotifyTokenExpiration) {
@@ -33,3 +34,23 @@ searchBtn.addEventListener('click', async () => {
   const spotifyResponseJson = await spotifyResponse.json();
   console.log(spotifyResponseJson);
 });
+
+function renderSearchResult(imageUrl, albumName, artistName, trackName) {
+  const imageElement = document.createElement('img');
+  imageElement.src = imageUrl;
+  const albumParagraph = document.createElement('p');
+  albumParagraph.innerText = `Album: ${albumName}`;
+  const artistParagraph = document.createElement('p');
+  artistParagraph.innerText = `Artist: ${artistName}`;
+  const trackParagraph = document.createElement('p');
+  trackParagraph.innerText = `Track: ${trackName}`;
+
+  const resultSect = document.createElement('section');
+  resultSect.classList.add('result');
+  resultSect.appendChild(imageElement);
+  resultSect.appendChild(albumParagraph);
+  resultSect.appendChild(artistParagraph);
+  resultSect.appendChild(trackParagraph);
+
+  searchResultsSect.appendChild(resultSect);
+}
