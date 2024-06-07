1e671b541a7b314c32932cf2a939e04617b564f6
UnGerardo <gerardojdls@gmail.com>
2024-06-06 18:17:08 -0700

    Offloaded spotify access_token ownership to the server, client sends search query to server and server makes the API call

--- controllers/server.mjs
+++ controllers/server.mjs
@@ -2,56 +2,79 @@ import 'dotenv/config';
 import { createServer } from 'node:http';
 import { readFile } from 'node:fs';
 
+let spotifyAccessToken = '';
+let spotifyTokenType = '';
+let spotifyTokenExpiration = 0;
+
 const server = createServer(async (req, res) => {
+  console.log(req.url);
+  console.log(req.method);
   let path;
 
-  if (req.url.includes('js')) {
-    res.setHeader('Content-Type', 'text/javascript');
-    console.log(req.url)
-    path = `.${req.url}`;
+  if (req.method === 'GET') {
+    if (req.url.includes('js')) {
+      res.setHeader('Content-Type', 'text/javascript');
+      path = `./js${req.url}`;
+    }
+    else if (req.url.includes('css')) {
+      res.setHeader('Content-Type', 'text/css');
+      path = `./css${req.url}`;
+    }
+    else {
+      res.setHeader('Content-Type', 'text/html');
+      path = './html/';
+
+      switch(req.url) {
+        // html
+        case '/':
+          res.statusCode = 200;
+          path += 'index.html';
+          break;
+        case '/spotSearch':
+          res.statusCode = 200;
+          path += 'spotSearch.html';
+          break;
+        default:
+          res.statusCode = 404;
+          path += '404.html';
+      }
+    }
   }
-  else if (req.url.includes('css')) {
-    res.setHeader('Content-Type', 'text/css');
-    console.log(req.url)
-    path = `.${req.url}`;
-  }
-  else {
-    res.setHeader('Content-Type', 'text/html');
-    path = './html/';
+  if (req.method === 'POST') {
+    switch (req.url.split('?')[0]) {
+      case '/searchTrack':
+        if (spotifyAccessToken === '' || Date.now() > spotifyTokenExpiration) {
+          await getSpotifyAccessToken();
+        }
 
-    switch(req.url) {
-      // html
-      case '/':
-        res.statusCode = 200;
-        path += 'index.html';
-        break;
-      case '/spotSearch':
-        res.statusCode = 200;
-        path += 'spotSearch.html';
-        break;
-
-      // endpoints
-      case '/spotToken':
-        const params = new URLSearchParams();
-        params.append('grant_type', 'client_credentials');
-        params.append('client_id', process.env.CLIENT_ID);
-        params.append('client_secret', process.env.CLIENT_SECRET);
-
-        const spotifyApiResponse = await fetch('https://accounts.spotify.com/api/token', {
-          method: 'POST',
-          body: params,
-          headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        let reqBody = '';
+        req.on('data', (chunk) => {
+          reqBody += chunk.toString();
         });
 
-        const spotifyApiJson = await spotifyApiResponse.json();
+        req.on('end', async () => {
+          const spotifySearchParams = new URLSearchParams();
+          spotifySearchParams.append('q', reqBody['searchQuery']);
+          spotifySearchParams.append('type', 'track');
+          spotifySearchParams.append('market', 'US');
+          spotifySearchParams.append('limit', 20);
+          spotifySearchParams.append('offset', 0);
 
-        console.log(spotifyApiJson);
-        res.end(JSON.stringify(spotifyApiJson));
+          const spotifyResponse = await fetch(`https://api.spotify.com/v1/search?${spotifySearchParams}`, {
+            method: 'GET',
+            headers: {'Authorization': `${spotifyTokenType} ${spotifyAccessToken}`}
+          });
+          const spotifyResponseJson = await spotifyResponse.json();
+
+          res.setHeader('Content-Type', 'application/json');
+          res.statusCode = 200;
+          res.end(JSON.stringify(spotifyResponseJson['tracks']));
+        });
         return;
-        break;
       default:
+        res.setHeader('Content-Type', 'text/html');
         res.statusCode = 404;
-        path += '404.html';
+        path = './html/404.html';
     }
   }
 
@@ -65,6 +88,25 @@ const server = createServer(async (req, res) => {
     res.end(data);
   });
 });
+
+async function getSpotifyAccessToken() {
+  const spotifyCredParams = new URLSearchParams();
+  spotifyCredParams.append('grant_type', 'client_credentials');
+  spotifyCredParams.append('client_id', process.env.CLIENT_ID);
+  spotifyCredParams.append('client_secret', process.env.CLIENT_SECRET);
+
+  const spotifyApiResponse = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    body: spotifyCredParams,
+    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+  });
+
+  const spotifyApiJson = await spotifyApiResponse.json();
+
+  spotifyAccessToken = spotifyApiJson['access_token'];
+  spotifyTokenType = spotifyApiJson['token_type'];
+  spotifyTokenExpiration = Date.now() + 3500000;
+}
 
 server.listen(3000, '127.0.0.1', () => {
   console.log('Listening on 127.0.0.1:3000');
--- js/script.js
+++ js/script.js
@@ -8,33 +8,17 @@ const searchQueryInput = document.getElementById('searchInput');
 const searchResultsSect = document.getElementById('searchResults');
 
 searchBtn.addEventListener('click', async () => {
-  if (spotifyAccessToken === '' || Date.now() > spotifyTokenExpiration) {
-    const response = await fetch('/spotToken');
-    const data = await response.json();
-    console.log(data);
-
-    spotifyAccessToken = data['access_token'];
-    spotifyTokenType = data['token_type'];
-    spotifyTokenExpiration = Date.now() + 3000000;
-  }
-
   const searchQuery = searchQueryInput.value;
 
-  const spotifyApiParams = new URLSearchParams();
-  spotifyApiParams.append('q', searchQuery);
-  spotifyApiParams.append('type', 'track');
-  spotifyApiParams.append('market', 'US');
-  spotifyApiParams.append('limit', 20);
-  spotifyApiParams.append('offset', 0);
-
-  const spotifyResponse = await fetch(`https://api.spotify.com/v1/search?${spotifyApiParams}`, {
-    method: 'GET',
-    headers: {'Authorization': `${spotifyTokenType} ${spotifyAccessToken}`}
+  const searchResponse = await fetch(`/searchTrack`, {
+    method: 'POST',
+    body: JSON.stringify({
+      searchQuery
+    })
   });
-  const spotifyResponseJson = await spotifyResponse.json();
-  // console.log(spotifyResponseJson);
+  const searchResponseJson = await searchResponse.json();
 
-  spotifyResponseJson['tracks']['items'].forEach(track => {
+  searchResponseJson['items'].forEach(track => {
     const imageUrl = track['album']['images'][1]['url'];
     const albumName = track['album']['name'];
     // can have multiple artists
