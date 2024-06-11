22c0387fca0d7c8923790cb10d9d77c552b1a47f
UnGerardo <gerardojdls@gmail.com>
2024-06-10 17:11:08 -0700

    Implemented button event listener to send POST req to /getSavedTracks

--- js/getPlaylists.js
+++ js/getPlaylists.js
@@ -2,13 +2,24 @@
 let spotifyAccessToken = '';
 let spotifyTokenType = '';
 
+const getSavedTracksBtn = document.getElementById('getSavedTracks');
+
 (async () => {
   const url = window.location.href;
   const urlParams = new URLSearchParams(url.split('?')[1]);
-  console.log(urlParams)
 
   const spotifyTokenResponse = await fetch(`/spotifyAuthToken?${urlParams}`);
   const spotifyTokenJson = await spotifyTokenResponse.json();
 
   ({ access_token: spotifyAccessToken, token_type: spotifyTokenType } = spotifyTokenJson);
+
+  getSavedTracksBtn.addEventListener('click', async () => {
+    const getSavedTracksReponse = await fetch(`/getSavedTracks`, {
+      method: 'POST',
+      body: JSON.stringify({
+        access_token: spotifyAccessToken,
+        token_type: spotifyTokenType
+      })
+    });
+  });
 })();
