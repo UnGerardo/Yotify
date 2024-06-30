6763a91f2da378aa34e9d19b7b5c4a117f58db54
UnGerardo <gerardojdls@gmail.com>
2024-06-29 18:13:51 -0700

    Playlists get shown when user authenticates with Spotify

--- public/js/getUserTracks.js
+++ public/js/getUserTracks.js
@@ -3,7 +3,7 @@ let SPOTIFY_ACCESS_TOKEN = '';
 let SPOTIFY_TOKEN_TYPE = '';
 let SPOTIFY_DISPLAY_NAME = '';
 
-const getSavedTracksBtn = document.getElementById('getSavedTracks');
+const $playlists = document.getElementById('playlists');
 
 (async () => {
   const url = window.location.href;
@@ -21,4 +21,48 @@ const getSavedTracksBtn = document.getElementById('getSavedTracks');
     token_type: SPOTIFY_TOKEN_TYPE,
     display_name: SPOTIFY_DISPLAY_NAME
   } = _spotifyTokenJson);
+
+  const _savedTracksParams = new URLSearchParams({
+    limit: 1,
+    offset: 0,
+    market: 'US'
+  });
+  const _savedTracksRes = await fetch(`https://api.spotify.com/v1/me/tracks?${_savedTracksParams}`, {
+    headers: { 'Authorization': `${SPOTIFY_TOKEN_TYPE} ${SPOTIFY_ACCESS_TOKEN}`}
+  }).then(res => res.json());
+
+  $renderPlaylist('liked_songs', 'Liked_Songs.png', 'Liked Songs', _savedTracksRes['total']);
+
+  const _playlistsParams = new URLSearchParams({
+    limit: 50,
+    offset: 0
+  });
+  const _playlistsRes = await fetch(`https://api.spotify.com/v1/me/playlists?${_playlistsParams}`, {
+    headers: { 'Authorization': `${SPOTIFY_TOKEN_TYPE} ${SPOTIFY_ACCESS_TOKEN}`}
+  }).then(res => res.json());
+
+  _playlistsRes['items'].forEach(playlist => {
+    $renderPlaylist(playlist['id'], playlist['images'][0]['url'], playlist['name'], playlist['tracks']['total']);
+  });
 })();
+
+function $renderPlaylist(id, imgUrl, name, trackCount) {
+  const $playlist = document.createElement('section');
+  $playlist.classList.add('playlist');
+  const $img = document.createElement('img');
+  $img.src = imgUrl;
+  const $nameP = document.createElement('p');
+  $nameP.innerText = name;
+  const $trackCountP = document.createElement('p');
+  $trackCountP.innerText = trackCount;
+  const $showBtn = document.createElement('button');
+  $showBtn.innerText = 'Show';
+  $showBtn.addEventListener('click', async () => {
+    if (id === 'liked_songs') {
+      return;
+    }
+  });
+
+  $playlist.append($img, $nameP, $trackCountP, $showBtn);
+  $playlists.appendChild($playlist);
+}
