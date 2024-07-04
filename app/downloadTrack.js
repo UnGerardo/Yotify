043a75c7309b9891c3e0ae0e068e2d666be1ffa3
UnGerardo <gerardojdls@gmail.com>
2024-07-03 21:52:21 -0700

    Added wait to playlist downloads to prevent too many requests error

--- app/downloadTrack.js
+++ app/downloadTrack.js
@@ -1,21 +1,25 @@
 
 const { parentPort } = require('worker_threads');
 const { spawn } = require('node:child_process');
+const { randomInt } = require('crypto');
 
 parentPort.on('message', (args) => {
-  console.log(`Worker started: ${args[0]} ${args[1][3]}`);
+  const wait = randomInt(30000, 60000);
+  setTimeout(() => {
+    console.log(`Worker started: ${args[0]} ${args[1][3]}`);
 
-  const spotdlInst = spawn(...args);
-  let STDOUT = '';
-  let STDERR = '';
+    const spotdlInst = spawn(...args);
+    let STDOUT = '';
+    let STDERR = '';
 
-  spotdlInst.stdout.on('data', (data) => {
-    STDOUT += data.toString();
-  });
-  spotdlInst.stderr.on('data', (data) => {
-    STDERR += data.toString();
-  });
-  spotdlInst.on('close', (code) => {
-    parentPort.postMessage({ code, STDOUT, STDERR});
-  });
+    spotdlInst.stdout.on('data', (data) => {
+      STDOUT += data.toString();
+    });
+    spotdlInst.stderr.on('data', (data) => {
+      STDERR += data.toString();
+    });
+    spotdlInst.on('close', (code) => {
+      parentPort.postMessage({ code, STDOUT, STDERR});
+    });
+  }, wait);
 });
