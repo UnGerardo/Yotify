228cfcbad10eb2af3e2b031739fbfc5bc4af0734
UnGerardo <gerardojdls@gmail.com>
2024-07-30 19:53:34 -0700

    Added new binaryModal to allow two choices with two callbacks

--- public/js/modal.js
+++ public/js/modal.js
@@ -15,3 +15,32 @@ function $createModal(text, callback = null) {
   $modalOverlay.appendChild($modal);
   document.body.appendChild($modalOverlay);
 }
+
+function $createBinaryModal(text, yesText, noText, yesCallback = null, noCallback = null) {
+  const $modalOverlay = $createElement('section', ['modal-overlay']);
+  const $modal = $createElement('section', ['modal']);
+  const $textP = $createElement('p', [], { innerText: text });
+  const $yesBtn = $createElement('button', ['btn'], { innerText: yesText });
+  const $noBtn = $createElement('button', ['btn'], { innerText: noText });
+
+  $yesBtn.addEventListener('click', () => {
+    document.body.removeChild($modalOverlay);
+    if (typeof yesCallback === 'function') {
+      yesCallback();
+    }
+  });
+
+  $noBtn.addEventListener('click', () => {
+    document.body.removeChild($modalOverlay);
+    if (typeof noCallback === 'function') {
+      noCallback();
+    }
+  });
+
+  const $btnContainer = $createElement('section');
+  $btnContainer.append($yesBtn, $noBtn);
+
+  $modal.append($textP, $btnContainer);
+  $modalOverlay.appendChild($modal);
+  document.body.appendChild($modalOverlay);
+}
