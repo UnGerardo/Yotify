db4a5e1b85489380994594ad3d4c912e932e1ab0
UnGerardo <gerardojdls@gmail.com>
2024-08-04 18:11:59 -0700

    Converted server.js to ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
-const express = require('express');
-const path = require('node:path');
+import express, { Request, Response } from 'express';
+import path from 'node:path';
 
-const spotifyRoutes = require('./routes/spotifyRoutes.js');
-const { SRC_DIR_PATH } = require('./constants.js');
+import spotifyRoutes from './routes/spotifyRoutes.js';
+import { PORT, SRC_DIR_PATH } from './constants.js';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port: number = PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,15 +15,14 @@ app.use(express.static(path.join(SRC_DIR_PATH, 'build/public')));
 app.use(express.static(path.join(SRC_DIR_PATH, 'public')));
 
 app.use('/spotify', spotifyRoutes);
-app.use('/favicon.ico', (req, res) => {
+app.use('/favicon.ico', (req: Request, res: Response) => {
   res.type('image/x-icon').sendFile(path.join(SRC_DIR_PATH, 'public/images/favicon.ico'));
 });
-app.use('/', (req, res) => {
+app.use('/', (req: Request, res: Response) => {
   res.sendFile(path.join(SRC_DIR_PATH, 'views/index.html'));
 });
 
-// middleware that handles 404 errors
-app.use(function(req, res, next) {
+app.use((req: Request, res: Response, next) => {
   res.status(404).sendFile(path.join(SRC_DIR_PATH, 'views/404.html'));
 });
 
