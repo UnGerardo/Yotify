38668a9522718d02b30ac85467b2d435fa728daa
UnGerardo <gerardojdls@gmail.com>
2024-08-05 18:21:23 -0700

    TS'ifed spotify workers

---
+++ src/zotifyWorker.ts
@@ -0,0 +1,36 @@
+
+import { parentPort } from 'worker_threads';
+import { spawn } from 'node:child_process';
+import { randomInt } from 'crypto';
+import { renameSync } from 'node:fs';
+import path from 'path';
+import { APP_DIR_PATH, ZOTIFY_DIR, ZOTIFY, ZOTIFY_ARGS, ZOTIFY_FORMAT, ZOTIFY_WAIT_MIN, ZOTIFY_WAIT_MAX } from './constants';
+import Track from './Track';
+
+if (parentPort) {
+  parentPort.on('message', (track: Track) => {
+    const wait: number = randomInt(ZOTIFY_WAIT_MIN, ZOTIFY_WAIT_MAX);
+    setTimeout(() => {
+      console.log(`Worker started: ${ZOTIFY} | ${track.url} | ${track.artists} | ${track.name}`);
+
+      const zotifyInst = spawn(...ZOTIFY_ARGS(track.url));
+      let STDOUT = '';
+      zotifyInst.stdout.on('data', (data) => STDOUT += data.toString());
+      let STDERR = '';
+      zotifyInst.stderr.on('data', (data) => STDERR += data.toString());
+
+      zotifyInst.on('close', (code) => {
+        const mainArtist = track.artists[0];
+        const expectedFilePath = path.join(APP_DIR_PATH, ZOTIFY_DIR, `${mainArtist}/${mainArtist} - ${track.name}.${ZOTIFY_FORMAT}`);
+        const desiredFilePath = path.join(APP_DIR_PATH, ZOTIFY_DIR, `${mainArtist}/${track.artists.join(', ')} - ${track.name}.${ZOTIFY_FORMAT}`);
+        renameSync(expectedFilePath, desiredFilePath);
+
+        if (parentPort){
+          parentPort.postMessage({ code, STDOUT, STDERR });
+        }
+      });
+    }, wait);
+  });
+} else {
+  throw new Error('ParentPort in spotdlWorker is null. Is DOWNLOAD_THREADS >= 1?');
+}
